bbc71929365825cc1bbbfb5d448189dcd67c0687
gbihry <98669194+gbihry@users.noreply.github.com>
2023-11-30 11:31:48 +0100

    fix: optimise animal with image

--- modele/animal.php
+++ modele/animal.php
@@ -59,36 +59,35 @@ class Animal extends database {
 
     public function addImage($idAnimal){
         // Test s'il n'y a pas d'erreur
-        if ($_FILES['photoAnimal']['tmp_name']){
-            if ($_FILES['photoAnimal']['error'] == 0)
-            {
-                // Test si la taille du fichier uploadé est conforme
-                if ($_FILES['photoAnimal']['size'] <= 5000000)
-                {
-                    // Test si l'extension du fichier uploadé est autorisée
-                    $infosfichier = new SplFileInfo($_FILES['photoAnimal']['name']);
-                    $extension_upload = $infosfichier->getExtension();
-                    $extensions_autorisees = array('jpg', 'png');
-                    $dir = dirname($_SERVER['SCRIPT_FILENAME']);
-                    if (in_array($extension_upload, $extensions_autorisees))
-                    {
-                        $uniqid = uniqid();
-                        $nomFichier = $uniqid.".".$extension_upload;
-                        // Stockage définitif du fichier photo dans le dossier uploads
-                        if (is_dir($dir."/photoAnimal")){
-                            move_uploaded_file($_FILES['photoAnimal']['tmp_name'], "photoAnimal/".$nomFichier);
-                        }else{
-                            mkdir($dir.'/photoAnimal');
-                        }
-                    }else{
-                        throw new Exception("Mauvaise extension");
-                    }
-                }else{
-                    throw new Exception("Fichier trop volumineux");
-                }
-            }else {
-                throw new Exception("Erreur de transfert");
-            }
+        if (!$_FILES['photoAnimal']['tmp_name']){
+            throw new Exception("Pas d'image");
+        }
+        if (!$_FILES['photoAnimal']['error'] == 0)
+        {
+            throw new Exception("Erreur de transfert");
+        }
+        // Test si la taille du fichier uploadé est conforme
+        if (!$_FILES['photoAnimal']['size'] >= 5000000)
+        {
+            throw new Exception("Fichier trop volumineux");
+        }
+
+        // Test si l'extension du fichier uploadé est autorisée
+        $infosfichier = new SplFileInfo($_FILES['photoAnimal']['name']);
+        $extension_upload = $infosfichier->getExtension();
+        $extensions_autorisees = array('jpg', 'png');
+        $dir = dirname($_SERVER['SCRIPT_FILENAME']);
+        if (!in_array($extension_upload, $extensions_autorisees))
+        {
+            throw new Exception("Mauvaise extension");
+        }
+        $uniqid = uniqid();
+        $nomFichier = $uniqid.".".$extension_upload;
+        // Stockage définitif du fichier photo dans le dossier uploads
+        if (is_dir($dir."/photoAnimal")){
+            move_uploaded_file($_FILES['photoAnimal']['tmp_name'], "photoAnimal/".$nomFichier);
+        }else{
+            mkdir($dir.'/photoAnimal');
         }
 
         $allImage = $this->getImagesAnimal($idAnimal);
@@ -124,35 +123,30 @@ class Animal extends database {
     public function createAnimal($nom, $age, $taille, $poid, $handicape, $idSPA, $idType){
         // Test s'il n'y a pas d'erreur
         $nomFichier = "default.jpg";
-        if ($_FILES['photoAnimal']['tmp_name']){
-            if ($_FILES['photoAnimal']['error'] == 0)
-            {
-                // Test si la taille du fichier uploadé est conforme
-                if ($_FILES['photoAnimal']['size'] <= 5000000)
-                {
-                    // Test si l'extension du fichier uploadé est autorisée
-                    $infosfichier = new SplFileInfo($_FILES['photoAnimal']['name']);
-                    $extension_upload = $infosfichier->getExtension();
-                    $extensions_autorisees = array('jpg', 'png');
-                    $dir = dirname($_SERVER['SCRIPT_FILENAME']);
-                    if (in_array($extension_upload, $extensions_autorisees))
-                    {
-                        $uniqid = uniqid();
-                        $nomFichier = $uniqid.".".$extension_upload;
-                        // Stockage définitif du fichier photo dans le dossier uploads
-                        if (is_dir($dir."/photoAnimal")){
-                            move_uploaded_file($_FILES['photoAnimal']['tmp_name'], "photoAnimal/".$nomFichier);
-                        }else{
-                            mkdir($dir.'/photoAnimal');
-                        }
-                    }else{
-                        throw new Exception("Mauvaise extension");
-                    }
-                }else{
-                    throw new Exception("Fichier trop volumineux");
-                }
-            }else {
+        if ($_FILES['photoAnimal']['tmp_name']) {
+            if (!$_FILES['photoAnimal']['error'] == 0) {
                 throw new Exception("Erreur de transfert");
+            }
+            // Test si la taille du fichier uploadé est conforme
+            if (!$_FILES['photoAnimal']['size'] >= 5000000) {
+                throw new Exception("Fichier trop volumineux");
+            }
+
+            // Test si l'extension du fichier uploadé est autorisée
+            $infosfichier = new SplFileInfo($_FILES['photoAnimal']['name']);
+            $extension_upload = $infosfichier->getExtension();
+            $extensions_autorisees = array('jpg', 'png');
+            $dir = dirname($_SERVER['SCRIPT_FILENAME']);
+            if (!in_array($extension_upload, $extensions_autorisees)) {
+                throw new Exception("Mauvaise extension");
+            }
+            $uniqid = uniqid();
+            $nomFichier = $uniqid . "." . $extension_upload;
+            // Stockage définitif du fichier photo dans le dossier uploads
+            if (is_dir($dir . "/photoAnimal")) {
+                move_uploaded_file($_FILES['photoAnimal']['tmp_name'], "photoAnimal/" . $nomFichier);
+            } else {
+                mkdir($dir . '/photoAnimal');
             }
         }
         
