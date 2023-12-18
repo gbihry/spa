bebc107f29e7b96bad67fbbd7e590ba7af173991
ThibaudRtz <thibaudreitzer39@gmail.com>
2023-12-18 15:30:24 +0100

    Add style on the signup page

--- vue/vueSignUp.php
+++ vue/vueSignUp.php
@@ -1,7 +1,7 @@
 <?php
 $title = TITREONGLET;
 $header = NOMSITE;
-$titre = "S'inscrire";
+$titre = "";
 
 $showHeader = false;
 
@@ -16,6 +16,13 @@ if(!isset($_SESSION['acces'])){
         echo('<p class="error">Les deux mot de passes ne correspondent pas</p>');
     }
     ?>
+<main class="signup_section">
+    <div class="left_signup">
+        <img src="./assets/login_img/login_background.png" alt="">
+    </div>
+
+    <div class="right_signup">
+        <h1>S'inscrire</h1>
     <form action=<?=$_SERVER['PHP_SELF']."?action=signup"?> method="POST">
         <div class="form_elt">
             <label for="">
@@ -53,14 +60,19 @@ if(!isset($_SESSION['acces'])){
                 <input type="password" name="mdp_verif" class="texte" required>
             </label>
         </div>
-        <input type="submit" class="valid" name="ok" value="valider">
+        <input type="submit" class="valid button_valid" name="ok" value="Valider">
     </form>
 
     <p>Vous avez déjà un compte ? <a class="lien" href="index.php?action=login">Se connecter</a></p>
     <a class="lien" href="index.php">Retour</a>
+    </div>
+    </main>
+    <div class="halo_div"></div>
+    <div class="halo_div2"></div>
     <?php
 }
 
 $contenu = ob_get_clean();
+$showFooter= false;
 $footer = "Sp-Hess";
 require "gabarit.php";
