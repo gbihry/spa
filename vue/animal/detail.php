cdb45c7f23cb3156282cdddf9167987d26a95691
ThibaudRtz <thibaudreitzer39@gmail.com>
2024-01-27 12:44:52 +0100

    ctrl + s the code for the style

--- vue/animal/detail.php
+++ vue/animal/detail.php
@@ -9,35 +9,50 @@ $dateArrivee = $ObjectAnimal->getDate($animal['dateArrivee']);
 ?>
 
 <section class="edit_page_admin_section">
-    <h2><?="Plus d'infos sur :" . $animal['nom']?></h2>
-    <div class="detail">
-        <p>Nom :
-           <span> <?= $animal['nom'] ?> 
-        </p>
-        <p>Age :
-           <span> <?= $animal['age'] ?> </span>
-        </p>
-        <p>Taille :
-           <span> <?= $animal['taille'] ?></span>
-        </p>
-        <p>Poids :
-           <span> <?= $animal['poid'] ?></span>
-        </p>
-        <p>Handicapé :
-           <span> <?= $animal['handicape'] == 1 ? "Oui" : "Non" ?></span>
-        </p>
-        <p>Arrivé le
-           <span> <?= $dateArrivee ?></span>
-        </p>
-        <h2>Photo(s) :</h2>
-        <?php
-        foreach ($imagesAnimal as $imageAnimal) {
-            ?>
-            <img src="photoAnimal/<?= $imageAnimal['nomImg'] ?>" alt="">
-            <?php
-        }
-        ?>
-    </div>
+   <h2>
+      <?= "Plus d'infos sur :" . $animal['nom'] ?>
+   </h2>
+   <div class="detail">
+      <div>
+         <p>Nom :
+            <span>
+               <?= $animal['nom'] ?>
+         </p>
+         <p>Âge :
+            <span>
+               <?= $animal['age'] ?>
+            </span>
+         </p>
+         <p>Taille :
+            <span>
+               <?= $animal['taille'] ?>
+            </span> cm
+         </p>
+         <p>Poids :
+            <span>
+               <?= $animal['poid'] ?>
+            </span> Kg
+         </p>
+         <p>Handicapé :
+            <span>
+               <?= $animal['handicape'] == 1 ? "Oui" : "Non" ?>
+            </span>
+         </p>
+         <p>Arrivé le :
+            <span>
+               <?= $dateArrivee ?>
+            </span>
+         </p>
+      </div>
+      <h2>Photo(s) :</h2>
+      <?php
+      foreach ($imagesAnimal as $imageAnimal) {
+         ?>
+         <img src="photoAnimal/<?= $imageAnimal['nomImg'] ?>" alt="">
+         <?php
+      }
+      ?>
+   </div>
 </section>
 <?php
 $contenu = ob_get_clean();
